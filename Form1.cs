c3d5929492d7cbade9a933a3acc4cc750b14faa5
PystyRobert <robertkiller52@gmail.com>
2023-12-18 18:13:58 +0200

    Calendar1

---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calendar1
+{
+    public partial class Form1 : Form
+    {
+        int month, year;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            displayDays();
+        }
+
+        private void displayDays() 
+        {
+            DateTime now = DateTime.Now;
+            month = now.Month;
+            year = now.Year;
+
+            String monthname = DateTimeFormatInfo.CurrentInfo.GetMonthName(month);
+            LBDATE.Text = monthname + " " + year;
+
+            DateTime startofthemonth = new DateTime(year, month, 1);
+
+            int days = DateTime.DaysInMonth(year, month);
+
+            int daysoftheweek = Convert.ToInt32(startofthemonth.DayOfWeek.ToString("d"));
+
+            for(int i=1; i < daysoftheweek; i++)
+            {
+                UserControlBlank ucblank = new UserControlBlank();
+                daycontainer.Controls.Add(ucblank);
+            }
+
+            for(int i=1; i <= days; i++)
+            {
+                UserControlDays ucdays = new UserControlDays();
+                ucdays.days(i);
+                daycontainer.Controls.Add(ucdays);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnprevious_Click(object sender, EventArgs e)
+        {
+            daycontainer.Controls.Clear();
+
+            month --;
+
+            if (month < 1)
+            {
+                month = 12;
+                year--;
+            }
+
+
+            String monthname = DateTimeFormatInfo.CurrentInfo.GetMonthName(month);
+            LBDATE.Text = monthname + " " + year;
+
+            DateTime startofthemonth = new DateTime(year, month, 1);
+
+            int days = DateTime.DaysInMonth(year, month);
+
+            int daysoftheweek = Convert.ToInt32(startofthemonth.DayOfWeek.ToString("d"));
+
+            for (int i = 1; i < daysoftheweek; i++)
+            {
+                UserControlBlank ucblank = new UserControlBlank();
+                daycontainer.Controls.Add(ucblank);
+            }
+
+            for (int i = 1; i <= days; i++)
+            {
+                UserControlDays ucdays = new UserControlDays();
+                ucdays.days(i);
+                daycontainer.Controls.Add(ucdays);
+            }
+        }
+
+        private void btnnext_Click(object sender, EventArgs e)
+        {
+            daycontainer.Controls.Clear();
+            month ++;
+            if(month >= 13)
+            {
+                month = 1;
+                year++;
+            }
+
+
+            String monthname = DateTimeFormatInfo.CurrentInfo.GetMonthName(month);
+            LBDATE.Text = monthname + " " + year;
+
+            DateTime startofthemonth = new DateTime(year, month, 1);
+
+            int days = DateTime.DaysInMonth(year, month);
+
+            int daysoftheweek = Convert.ToInt32(startofthemonth.DayOfWeek.ToString("d"));
+
+            for (int i = 1; i < daysoftheweek; i++)
+            {
+                UserControlBlank ucblank = new UserControlBlank();
+                daycontainer.Controls.Add(ucblank);
+            }
+
+            for (int i = 1; i <= days; i++)
+            {
+                UserControlDays ucdays = new UserControlDays();
+                ucdays.days(i);
+                daycontainer.Controls.Add(ucdays);
+            }
+        }
+    }
+}
